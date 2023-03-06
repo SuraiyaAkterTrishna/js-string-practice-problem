7cc6c32cac81803dba3348c8d9c2f93aaf7c5b07
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-06 23:12:41 +0600

    string concat

---
+++ string-concat.js
@@ -0,0 +1,13 @@
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1.concat(' ', str2));
+// Expected output: "Hello World"
+
+console.log(str2.concat(', ', str1));
+// Expected output: "World, Hello"
+
+const firstName = "Kamrujjaman";
+const lastName = "Rony";
+
+console.log(firstName.concat(', ',lastName));
