e84db1e5686df0a9f818275024d28f236e876ec9
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-07 21:25:27 +0600

    string letter can be accessed by the index number

--- string.js
+++ string.js
@@ -1,4 +1,10 @@
 let myAddress = '3/6 KDA Avenue, Khulna, Bangladesh';
-console.log(myAddress.length);
+// console.log(myAddress.length);
 myAddress = 'Rupsa Bridge, Bokharia Mosque, Khulna';
-console.log(myAddress.length);
+// console.log(myAddress.length);
+console.log(myAddress[7]);
+/* 
+myAddress[7] = 'a';
+console.log(myAddress[7]); 
+This is not possible because string is immutable;
+*/
