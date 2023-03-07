750db655841c5c9d5009e652531585bbaec02d94
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-07 21:22:41 +0600

    string have length

--- string.js
+++ string.js
@@ -1,3 +1,4 @@
 let myAddress = '3/6 KDA Avenue, Khulna, Bangladesh';
+console.log(myAddress.length);
 myAddress = 'Rupsa Bridge, Bokharia Mosque, Khulna';
-console.log(myAddress);
+console.log(myAddress.length);
